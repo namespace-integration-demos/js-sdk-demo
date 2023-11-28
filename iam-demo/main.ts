210f8cea30e1f1ac9ab88267dc2597552cefacb5
Kirill Nikolaev <kirill@namespacelabs.com>
2023-11-28 19:00:59 +0100

    Update to use ensureTenantForExternalAccount.

--- iam-demo/main.ts
+++ iam-demo/main.ts
@@ -7,9 +7,10 @@ const partnerOidcIssuer = "partner.example.com";
 const partnerOidcKey = `-----BEGIN EC PRIVATE KEY-----
 ...
 -----END EC PRIVATE KEY-----`;
+const partnerOidcKeyId = "...";
 
 // Allocated by Namespace team:
-const namespacePartnerId = "user_01h6dvjymxsh7absdzhkfz9v6f";
+const namespacePartnerId = "...";
 
 void main();
 
@@ -18,6 +19,7 @@ async function main() {
 	console.log("Getting an identity token...");
 	const rawToken = jwt.sign({}, partnerOidcKey, {
 		algorithm: "ES256",
+		keyid: partnerOidcKeyId,
 		expiresIn: "20m",
 		issuer: partnerOidcIssuer,
 		audience: "namespace.so",
@@ -33,8 +35,9 @@ async function main() {
 	// Create instance.
 	// endUserId is an string opaque to Namespace.
 	console.log("Creating a new tenant...");
-	const createResp = await client.createTenant({
-		visibleName: "example tenant",
+	const createResp = await client.ensureTenantForExternalAccount({
+		visibleName: "example",
+		externalAccountId: "example@namespacelabs.com",
 	});
 	const tenantId = createResp.tenant.id;
 	console.log("   - ID:", tenantId);
